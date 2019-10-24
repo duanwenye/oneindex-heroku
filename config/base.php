75f7704ffc10496e12b0debd0b4e24eeab278aac
duanwenye <56856406+duanwenye@users.noreply.github.com>
2019-10-25 00:22:43 +0800

    Update base.php

--- config/base.php
+++ config/base.php
@@ -1,9 +1,9 @@
 <?php return array (
   'site_name' => 'OneIndex',
   'password' => 'oneindex',
-  'style' => 'material',
+  'style' => 'nexmoe',
   'onedrive_root' => '',
-  'cache_type' => 'secache',
+  'cache_type' => 'filecache',
   'cache_expire_time' => 3600,
   'cache_refresh_time' => 600,
   'root_path' => '?',
